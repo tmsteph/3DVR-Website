50846b5939782f3a9b40f171a8f5a93416ebc422
Thomas Stephens <tmsteph1290@gmail.com>
2022-07-08 14:57:34 -0500

    strangely Spinning logo and logo label

--- 3DVR/src/App.js
+++ 3DVR/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './3DVR.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        3DVR
         </p>
         <a
           className="App-link"
