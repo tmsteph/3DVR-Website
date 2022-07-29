af8114c21c5f814344afbd92a9287e11c6de93ff
Thomas Stephens <tmsteph1290@gmail.com>
2022-07-28 23:25:43 -0500

    Made some spaces for readability

--- 3DVR/src/App.js
+++ 3DVR/src/App.js
@@ -5,10 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+	  
         <img src={logo} className="App-logo" alt="logo" />
+
         <p className="Catch-phrase">
         Build The Future
         </p>
+
       </header>
     </div>
   );
