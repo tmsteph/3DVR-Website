b519e949fa746e10d595c5f0a5c9a5505253c738
Thomas Stephens <tmsteph1290@gmail.com>
2022-08-15 11:36:12 -0700

    looking good working on colors

--- 3DVR/src/App.js
+++ 3DVR/src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-	  
+      <header  className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        
+        <p className= "Productions">
+          Productions
+        </p>
         <p className="Catch-phrase">
         Build The Future
         </p>
-
+        
       </header>
     </div>
   );
