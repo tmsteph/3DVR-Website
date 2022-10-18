5b9521a6065d54193403a489c0711f2bb091cc31
Thomas Stephens <tmsteph1290@gmail.com>
2022-10-17 19:19:45 -0700

    made a black squaregit add App.js !

--- 3DVR/src/App.js
+++ 3DVR/src/App.js
@@ -1,4 +1,6 @@
 /* Ignore the imports */ 
+import ReactDOM from 'react-dom'
+import { Canvas } from '@react-three/fiber'
 
 import logo from './3DVR.png';
 import './App.css';
@@ -81,39 +83,16 @@ function App() {
         </div>
         
         {/* ----------------- Three.js section ----------*/}
+        <div id="canvas-container">
+        <Canvas>
+  <mesh>
+    <boxGeometry />
+    <meshStandardMaterial />
+  </mesh>
+  </Canvas>
+        </div>
 
-import * as THREE from 'three';
-
-const scene = new THREE.Scene();
-
-
-componentDidMount() {
-    // === THREE.JS CODE START ===
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-    camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    };
-    animate();
-    // === THREE.JS EXAMPLE CODE END ===
-  }
-  render() {
-    return (
-      <div />
-    )
-  }
-}
+        {/* ReactDOM.render(<App />, document.getElementById('root')) */}
 
 
         {/* -----------------  Biography    ------------ */}
@@ -149,7 +128,7 @@ componentDidMount() {
 
               he is ready to pay attention to the details that make 
               
-              any experience believable, relatable, and polished.              
+
             </p>
             
             </div>
